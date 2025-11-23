aac9ebb33f5d2913b17b85b7b507dddd4596b102
Aestheticsuraj234 <jhapriya458@gmail.com>
2025-11-23 21:11:17 +0530

    completed

--- client/mdx-components.tsx
+++ client/mdx-components.tsx
@@ -1,10 +1,14 @@
 import defaultMdxComponents from 'fumadocs-ui/mdx';
 import type { MDXComponents } from 'mdx/types';
 import { APIPage } from '@/components/api-page';
+import { Tab, Tabs } from 'fumadocs-ui/components/tabs';
+
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
     ...defaultMdxComponents,
     APIPage,
+    Tab,
+    Tabs,
     ...components,
   };
 }
